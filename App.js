b4e02c747e88b8c139116c75f2d6b58c3bfafa28
German Vassiljev <93324304+GermanVassiljev@users.noreply.github.com>
2023-09-20 16:31:22 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,103 @@
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const idRef = useRef();
+  const nameRef = useRef();
+  const priceRef = useRef();
+  const imageRef = useRef();
+  const stockRef = useRef();
+  const catIdRef = useRef();
+  const isActiveRef = useRef();
+  const summString = null;
+  const [isUsd, setUsd] = useState(false);
+
+  useEffect(() => {
+    fetch("https://localhost:7049/api/Products")
+      .then(res => res.json())
+      .then(json => setProducts(json));
+  }, []);
+
+  function kustuta(index) {
+    fetch("https://localhost:7049/api/Products/kustuta/" + index, {"method": "DELETE"})
+      .then(res => res.json())
+      .then(json => setProducts(json));
+  }
+
+  function tellida(index) {
+    let summString = products[index].price.toString();
+    fetch("https://localhost:7049/api/Products/pay/" + summString, {"method": "GET"})
+      .then(res => res.json())
+      .then(json => setProducts(json));
+  }
+
+  function lisa() {
+    fetch(`https://localhost:7049/api/Products/lisa/${Number(idRef.current.value)}/${nameRef.current.value}/${imageRef.current.value}/${Number(priceRef.current.value)}/${isActiveRef.current.checked}/${Number(stockRef.current.value)}/${Number(catIdRef.current.value)}`, {"method": "POST"})
+      .then(res => res.json())
+      .then(json => setProducts(json));
+  }
+
+  function dollariteks() {
+    const kurss = 1.1;
+    setUsd(true);
+    fetch("https://localhost:7049/api/Products/hind-dollaritesse/" + kurss, {"method": "PATCH"})
+      .then(res => res.json())
+      .then(json => setProducts(json));
+  }
+
+  function eurodeks() {
+    const kurss = 0.9091;
+    setUsd(false);
+    fetch("https://localhost:7049/api/Products/hind-eurosse/" + kurss, {"method": "PATCH"})
+      .then(res => res.json())
+      .then(json => setProducts(json));
+  }
+
+  return (
+    <div className="App">
+      <label>ID</label> <br />
+      <input ref={idRef} type="number" /> <br />
+      <label>Name</label> <br />
+      <input ref={nameRef} type="text" /> <br />
+      <label>Price</label> <br />
+      <input ref={priceRef} type="number" /> <br />
+      <label>Stock</label> <br />
+      <input ref={stockRef} type="number" /> <br />
+      <label>Image</label> <br />
+      <input ref={imageRef} type="text" /> <br />
+      <label>Category Nr</label> <br />
+      <input ref={catIdRef} type="number" /> <br />
+      <label>Active</label> <br />
+      <input ref={isActiveRef} type="checkbox" /> <br />
+      <button onClick={() => lisa()}>Lisa</button>
+      <br/>
+      {isUsd === false && <button onClick={() => dollariteks()}>Muuda dollariteks</button>}
+      {isUsd === true && <button onClick={() => eurodeks()}>Muuda eurodeks</button>}
+      {products.map((product, index) => 
+        <table>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Stock</th>
+            <th>Category Nr</th>
+            <th>Image</th>
+            <th>Delete</th>
+            <th>Buy</th>
+          </tr>
+          
+          <td>{product.id}</td>
+          <td>{product.name}</td>
+          <td>{product.price.toFixed(2)}</td>
+          <td>{product.stock}</td>
+          <td>{product.categoryId}</td>
+          <td>{product.image}</td>
+          <td><button onClick={() => kustuta(index)}>x</button></td>
+          <td><button onClick={() => tellida(index)}>Pay</button></td>
+        </table>)}
+    </div>
+  );
+}
+
+export default App;
